9a5c67e1e28d1c3b5f27916edd694d5f6bd3107e
pkwyy <bimboadejo@gmail.com>
2025-01-19 18:22:28 +0100

    Add util

---
+++ src/utils/variants.js
@@ -0,0 +1,21 @@
+export const containerVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+}
+
+export const itemVariants = {
+  hidden: { opacity: 0, y: -50 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+}
