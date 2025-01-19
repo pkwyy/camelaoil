e9cca1e331d76c252a9c19575e10b3291f49fc0c
pkwyy <bimboadejo@gmail.com>
2025-01-19 17:35:20 +0100

    Add pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div className="">
+      Home hefsuidh dsuijds iusahn uasdjbnadsfniasdi
+      uasfhnuiaesdfiuvneasidfvusoidfuvnjusdfvnusjdnfvuinsdeiofvoisedjfoivsdoj
+      kjnsdfjnkfs jdsfnvjoisdfv efjdsnjeiods dfjnsjioewd uineijf iudsjnfgujesd
+      uijndsuf iunjwdesuofjrn iuewdsjnfuse uijnesduoirf unesdfuioesd iuojdsfn
+      iujsadfjioiesojdfio sudfjniosfde uinsdijfo iudjsf ujdsf iujsdijf
+      uijnsdfijngvk sunijdklsfjnvp
+    </div>
+  )
+}
+
+export default Home
