60f0409ef0fa82c159dae88d2e18d538a458f51e
pkwyy <bimboadejo@gmail.com>
2025-01-19 17:34:44 +0100

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { Route, Routes } from "react-router"
 import "./App.css"
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import Products from "./pages/Products"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
 
 function App() {
-  return <div>Camela Oil</div>
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
